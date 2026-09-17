252815c5b0ca6865953047a0d6ac9dd5838a5cc7
LeahJKH <leahjb06.kodehode@gmail.com>
2026-09-17 10:20:37 +0200

    js cards

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 const url = "https://www.freetogame.com/api/games?"
 const containGame = document.querySelector("#gameContain")
 
-
-
 async function getData(apiurl) {
     try {
         fetch(apiurl)
@@ -25,32 +23,37 @@ async function getData(apiurl) {
 
 function createCard(arrayData) {
     arrayData.forEach(apiData => {     
-        const div  = document.createElement("div")
+        const div = document.createElement("div")
+        div.className = "gameCard"
         
         const pDev = document.createElement("p")
         const devtxt = document.createTextNode(apiData.developer)
         pDev.append(devtxt)
-        
+        pDev.className = "devTxt"
+
         const pgenre = document.createElement("p")
         const genretxt = document.createTextNode(apiData.genre)
         pgenre.append(genretxt)
-        
+        pgenre.className = "genreTxt"
+
         const titleh3 = document.createElement("h3")
         const titletxt = document.createTextNode(apiData.title)
         titleh3.append(titletxt)
-        
+        titleh3.className = "title"
+
         const descp = document.createElement("p")
         const desctxt = document.createTextNode(apiData.short_description)
         descp.append(desctxt)
-        
-        const img = document.querySelector("img")
+        descp.className = "descTxt"
+
+        const figure = document.createElement("figure")
+        const img = document.createElement("img")
         img.src = apiData.thumbnail
+        figure.appendChild(img)
+        figure.className = "imgContain"
         
-        div.appendChild(titleh3)
-        div.appendChild(img)
-        div.appendChild(descp)
-        div.appendChild(pgenre)
-        div.appendChild(pDev)
+        div.append(titleh3, figure, descp, pgenre, pDev)
+
         
         containGame.appendChild(div)
     });
